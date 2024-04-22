3f33ae39fa97a760452f8c87920f91e3ac01b1ae
kralickoss <144122015+kralickoss@users.noreply.github.com>
2024-04-22 10:14:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+// Start PHP session or include any necessary PHP scripts if needed
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TFKPTGEZH0"></script>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Kentucky Fried Chicken</title>
+    <style>
+        @font-face {font-family: Consolas; src: url(https://kfc.cz/);}
+
+        body {
+            background-repeat: no-repeat;
+            background-size: 1680px 505px;
+            background-position: top;
+            background-image:url("Obrázky/kfc-gif.gif");
+            background-color: rgb(173, 170, 164);
+        }
+
+    </style>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <script src='main.js'></script>
+
+    <script type="text/javascript" src="https://www.termsfeed.com/public/cookie-consent/4.1.0/cookie-consent.js" charset="UTF-8"></script>
+    <script type="text/javascript" charset="UTF-8">
+    document.addEventListener('DOMContentLoaded', function () {
+    cookieconsent.run({"notice_banner_type":"simple","consent_type":"express","palette":"dark","language":"cs","page_load_consent_levels":["strictly-necessary"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"page_refresh_confirmation_buttons":false});
+    });
+    </script>
+
+    <noscript>Free cookie consent management tool by <a href="https://www.termsfeed.com/">TermsFeed</a></noscript>
+
+</head>
+<body>
+
+    <div class="bek" title="Go back to Main Page">
+        <a href="index.php" class="bek"><img src="Obrázky/Kfc_logo.png" alt="Logo" class="logos"></a>
+    </div>
+ 
+    <div class="centruj"><a href="https://kfc.cz/" class="e"><div class="center"><img src="Obrázky/KFC-logo-2014.png" title="Go to official sites of KFC" width="680" height="280" alt="logo-ofiko" class="obrazek"></div></a></div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <div class="centerTlacitka">
+        <a class="padingos button button1" href="historie.html">Historie</a>
+        <a class="padingos button button2" href="restaurace.html">Restaurace</a>
+        <a class="padingos button button2" href="jidelní-listek.html">Jídelní lístek</a>
+        <a class="padingos button button1" href="kontakty.html">Kontakty</a>
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <div class="center">
+        <img src="Obrázky/kfc-fast-food.gif" alt="kfc-restaurace" width="1700" height="1000">
+        <br>
+        <img src="Obrázky/mr-bean-kfc.gif" alt="kfc-mrbean" width="1700" height="1000">
+    </div>
+
+    <footer>
+        <div class="footer-content">
+            <h3><a href="privacy.html">Ochrana soukromí</a></h3>
+        </div>
+    </footer>
+
+</body>
+</html>
