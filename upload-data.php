65c4d327398ce51acf70808929c10714cc7bf259
kralickoss <144122015+kralickoss@users.noreply.github.com>
2024-05-24 12:34:33 +0200

    Add files via upload

---
+++ upload-data.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = $conn->real_escape_string($_POST['jmeno']);
+    $lastname = $conn->real_escape_string($_POST['prijmeni']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $subject = $conn->real_escape_string($_POST['subject']);
+    $message = $conn->real_escape_string($_POST['zprava']);
+
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email, subject, message)
+            VALUES ('$firstname', '$lastname', '$email', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
